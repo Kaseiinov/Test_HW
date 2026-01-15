94c0799be58e6a1a793d5129d436d0a7474d23ca
Kaseiinov_I <izlax6002@gmail.com>
2026-01-15 19:11:15 +0600

    first commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import org.apache.commons.math3.util.Precision;
+import org.example.test.Test;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    static void main() {
+
+
+    }
+
+}
---
+++ src/main/java/org/example/test/Test.java
@@ -0,0 +1,6 @@
+package org.example.test;
+
+public class Test {
+    protected String name;
+    String lastName;
+}
